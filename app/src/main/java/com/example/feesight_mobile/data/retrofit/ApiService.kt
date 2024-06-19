249c6ec0951c1428cad4eb701c118fe352001814
aflinxh <alfinalamsyahhh@gmail.com>
2024-06-19 18:57:26 +0700

    GET transactions by date

--- app/src/main/java/com/example/feesight_mobile/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/feesight_mobile/data/retrofit/ApiService.kt
@@ -3,13 +3,14 @@ package com.example.feesight_mobile.data.retrofit
 import com.example.feesight_mobile.data.response.InvestResponse
 import com.example.feesight_mobile.data.response.TransactionsResponse
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ApiService {
     @GET("user/transactions")
     fun getTransactions(): Call<TransactionsResponse>
+
+    @GET("user/transactions")
+    fun getTransactionsByDate(@Query("date") date: String): Call<TransactionsResponse>
 
     @POST("predict")
     fun predict(@Body request: Map<String, String>): Call<InvestResponse>
